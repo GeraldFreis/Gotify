f67b2038ac0ed9b51d610e89b4716258fa37faa9
GeraldFreis <91832029+GeraldFreis@users.noreply.github.com>
2021-12-07 16:09:42 +1030

    commit on master

--- searching_feature_frontend.py
+++ searching_feature_frontend.py
@@ -35,7 +35,7 @@ class Searching:
         self.large_font = font.Font(family="Gothic Medium", size=400)
         self.medium_font = font.Font(family="Gothic Medium", size=20)
         self.small_font = font.Font(family="Gothic Medium", size=10)
-        
+
         """Tkinter features"""
 
         # centering frame
@@ -43,32 +43,45 @@ class Searching:
         # width=300, height=150,
         # bg=deep_black)
 
-        self.gotify_image_frame = tk.Frame(master=self.main_search_window,
-        border=0,
-        bg=deep_black)
+
+        # frames
+        """Setting up features"""   
+        
+        main_font = font.Font(family="Gothic Medium", size=40)
+        new_main_font = font.Font(family="Gothic Medium", size=100)
+
+        self.frame_to_center = tk.Frame(master=self.main_search_window, bg=deep_black, width=400, height=50, border=0)
+        
+        # frame for labels
+        
+        self.search_label_frame = tk.Frame(master=self.main_search_window, bg=deep_black, width=400, height=200, border=0)
+
 
         # Gotify Label and image
         try:
             # resizing image
             self.spotify_icon_img = (Image.open(fp=r"images\spotify-logo.png"))
-            self.spotify_icon_img = self.spotify_icon_img.resize((400,300), Image.ANTIALIAS)
-            self.spotify_icon_img = ImageTk.PhotoImage(master=self.gotify_image_frame, image=self.spotify_icon_img)
+            self.spotify_icon_img = self.spotify_icon_img.resize((440,325), Image.ANTIALIAS)
+            self.spotify_icon_img = ImageTk.PhotoImage(master=self.main_search_window, image=self.spotify_icon_img)
         
         except FileNotFoundError or FileExistsError:
             print("You do not have the necessary assets in this directory")
         
 
-        self.gotify_label = tk.Label(master=self.main_search_window,
+        self.gotify_label = tk.Label(master=self.search_label_frame,
         text="Gotify",
-        font=self.large_font,
+        font=new_main_font,
         bg=deep_black,
         fg=unhighlighted_text,
         border=0)
 
-        self.gotify_image_button = tk.Button(master=self.gotify_image_frame,
-        image=self.spotify_icon_img,
+        """Aesthetics"""
+        self.spotify_logo_icon = tk.Button(master=self.main_search_window,
         bg=deep_black,
-        border=0)
+        image=self.spotify_icon_img,
+        border=0,
+        height=325,
+        width=400)
 
 
         # guide label
@@ -81,7 +94,7 @@ class Searching:
         height=2,
         border=0)
 
-        # search label
+        # # search label
         self.search_label = tk.Label(master=self.main_search_window,
         text='Search: ',
         font=self.medium_font,
@@ -89,7 +102,7 @@ class Searching:
         bg=deep_black,
         border=0)
 
-        # search entry
+        # # search entry
         self.entry_contents = tk.StringVar()
         self.entry_field = tk.Entry(master=self.main_search_window,
         textvariable=self.entry_contents,
@@ -123,25 +136,57 @@ class Searching:
         fg=unhighlighted_text,
         bg=deep_black,
         border=1)
-        
-    
-    def apply(self):
+
+        """applying everything"""
         """Tkinter features"""
 
         # centering frame
         # self.centering_frame.grid(row=0, column=0)
 
-        # other frames
-        self.gotify_image_frame.grid(row=0, column=0)
+        self.search_label_frame.grid(row=0, column=2, columnspan=2, rowspan=4)
         
-        # other features
-        self.gotify_label.grid(row=0, column=1, rowspan=3, columnspan=4)
-        # gotify image
-        self.gotify_image_button.grid(row=0, rowspan=2, column=0, columnspan=2)
-        self.guide_label.grid(row=4, column=2, rowspan=2)
-        self.search_label.grid(row=6, column=1)
-        self.entry_field.grid(row=6, column=2, columnspan=3)
-        self.enter_button.grid(row=8, column=2, columnspan=2)
+        self.frame_to_center.grid(row=0, rowspan=3, column=1, columnspan=1)
+        self.gotify_label.grid(row=0, column=1, rowspan=2, columnspan=3)
+        self.spotify_logo_icon.grid(row=0, column=1, rowspan=2, columnspan=1)
+
+        self.guide_label.grid(row=3, column=2, columnspan=1)
+        self.search_label.grid(row=4, column=1)
+        self.entry_field.grid(row=4, column=2)
+        self.enter_button.grid(row=5, column=3)
+        
+    
+    # def apply(self):
+    #     """Tkinter features"""
+
+    #     # centering frame
+    #     # self.centering_frame.grid(row=0, column=0)
+        
+    #     self.frame_to_center.grid(row=0, rowspan=3, column=1, columnspan=1)
+    #     self.gotify_label.grid(row=1, column=3)
+    #     self.spotify_logo_icon.grid(row=0, column=1, rowspan=2, columnspan=1)
+
+    #     self.guide_label.grid(row=3, column=2, columnspan=1)
+    #     self.search_label.grid(row=4, column=1)
+    #     self.entry_field.grid(row=4, column=2)
+    #     self.enter_button.grid(row=5, column=3)
+
+
+
+        # other frames
+        # self.gotify_label_frame.grid(row=0, column=3, rowspan=3, columnspan=4)
+        # self.gotify_image_frame.grid(row=0, column=0, rowspan=3, columnspan=4)
+        
+        # # other features
+
+        # # gotify label
+        # self.gotify_label.grid(row=0, column=5, rowspan=3, columnspan=4)
+        # # gotify image
+        # self.gotify_image_button.grid(row=0, rowspan=2, column=2, columnspan=2)
+
+        # self.guide_label.grid(row=4, column=5, rowspan=2)
+        # self.search_label.grid(row=6, column=5)
+        # self.entry_field.grid(row=6, column=6, columnspan=3)
+        # self.enter_button.grid(row=8, column=5, columnspan=2)
 
         # screen update
         self.main_search_window.mainloop()
